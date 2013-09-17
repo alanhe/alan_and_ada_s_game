9cb24274340c7bcc210b6524153cfdaab4a07cd4
AlanHe <AlanHe@zhenghe_2006@hotmail.com>
2013-09-17 12:22:38 +0800

    1. AMD using curl.js. 2. Add update logs.

---
+++ mod/eventEmitter.js
@@ -0,0 +1,35 @@
+define(function () {
+	return {
+		_listeners: {},
+		
+		on: function(event, listener){
+			var listeners = this._listeners[event] || [];
+			listeners.push(listener);
+			this._listeners[event] = listeners;
+		},
+		
+		off: function(event, listener){
+			var listeners = this._listeners[event];
+			if(listeners){
+				//FIXME: How to compare two functions equal?
+				listener = listener.toString();
+				for(var i = listeners.length - 1; i > -1; --i){
+					if(listeners[i].toString() === listener){
+						return listeners[i];
+					}
+				}
+			}
+		},
+		
+		emit: function(event){
+			var args = Array.prototype.slice.call(arguments, 1),
+				listeners = this._listeners[event];
+			
+			if(listeners){
+				for(var i = listeners.length - 1; i > -1; --i){
+					listeners[i](args);
+				}
+			}
+		}
+	};
+});
