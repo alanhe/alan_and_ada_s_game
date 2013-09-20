efac6e41928faf207eaa415b87cc4b3669f0398a
AlanHe <zhenghe_2006@hotmail.com>
2013-09-21 00:39:33 +0800

    Fix for Timer does not stop.

--- mod/Timer.js
+++ mod/Timer.js
@@ -4,28 +4,19 @@ define(function(){
 	exports.newTimer = function(progback, timeInMillis){
 		var timer = {
 			handler: -1,
-			interrupt: function(){
-				if(this.handler !== -1){
-					clearTimeout(this.handler);
-					this.handler = -1;
-				}
-			},
 			bundle: {
 				count: 0
 			},
 			stop: function(){
-				clearTimeout(this.handler);
-				this.handler = -1;
+				clearInterval(timer.handler);
+				timer.handler = -1;
 			}
 		};
 
-		var timerCallback = function(){
+		timer.handler = setInterval(function(){
 			timer.bundle.count += 1;
 			progback(timer.bundle);
-			timer.handler = setTimeout(timerCallback, timeInMillis);
-		};
-
-		timer.handler = setTimeout(timerCallback, timeInMillis);
+		}, timeInMillis);
 		return timer;
 	};
 
